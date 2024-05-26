7fd46c3715304bf636a1a6f108cfbf5042a6c50f
jeanbarbosa <programmer.jean@gmail.com>
2024-05-26 00:43:23 -0300

    chore: create menu with buttomsheet

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,59 +1,95 @@
+import { useRef } from "react"
 import { theme } from "@/theme"
 import { Tabs } from "expo-router"
+import { View } from "react-native"
+import { Menu } from "@/components/Menu"
 import { Avatar } from "@/components/Avatar"
-import { Foundation, Ionicons } from "@expo/vector-icons"
+import BottomSheet from "@gorhom/bottom-sheet"
+import { FontAwesome5, Foundation, Ionicons } from "@expo/vector-icons"
 
 export default function TabLayout() {
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  const handleBootomsheetOpen = () => {
+    bottomSheetRef.current?.expand()
+  }
+
+  const handleBootomsheetClose = () => {
+    bottomSheetRef.current?.snapToIndex(0)
+  }
+
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: theme.colors.white,
-        tabBarInactiveTintColor: theme.colors.gray[600],
-        tabBarStyle: {
-          backgroundColor: theme.colors.black,
-          borderColor: theme.colors.black,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ size, color }) => {
-            return <Foundation name="home" size={size} color={color} />
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: theme.colors.white,
+          tabBarInactiveTintColor: theme.colors.gray[600],
+          tabBarStyle: {
+            backgroundColor: theme.colors.black,
+            borderColor: theme.colors.black,
           },
         }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarIcon: ({ size, color }) => {
-            return <Ionicons name="search" size={size} color={color} />
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="messages"
-        options={{
-          tabBarIcon: ({ size, color }) => {
-            return (
-              <Ionicons name="chatbubble-ellipses" size={size} color={color} />
-            )
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Avatar
-              selected={color == theme.colors.white}
-              source={{ uri: "https://github.com/jeanbarbosa.png" }}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <Foundation name="home" size={size} color={color} />
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <Ionicons name="search" size={size} color={color} />
+            },
+          }}
+        />
+
+        <Tabs.Screen
+          name="menu"
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return <FontAwesome5 name="plus" size={size} color={color} />
+            },
+          }}
+          listeners={() => ({
+            tabPress: (e) => {
+              e.preventDefault()
+              handleBootomsheetOpen()
+            },
+          })}
+        />
+        <Tabs.Screen
+          name="messages"
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return (
+                <Ionicons
+                  name="chatbubble-ellipses"
+                  size={size}
+                  color={color}
+                />
+              )
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Avatar
+                selected={color == theme.colors.white}
+                source={{ uri: "https://github.com/jeanbarbosa.png" }}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+      <Menu ref={bottomSheetRef} onClose={handleBootomsheetClose} />
+    </View>
   )
 }
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View } from "react-native"
+import BottomSheet from "@gorhom/bottom-sheet"
+import { forwardRef } from "react"
+import { FontAwesome } from "@expo/vector-icons"
+import { styles } from "./styles"
+import { theme } from "@/theme"
+
+export const Menu = forwardRef<BottomSheet, MenuProps>(({ onClose }, ref) => {
+  return (
+    <BottomSheet
+      ref={ref}
+      index={0}
+      snapPoints={[0.01, 230]}
+      backgroundStyle={styles.container}
+      handleComponent={() => null}
+    >
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <FontAwesome
+            name="close"
+            onPress={onClose}
+            size={24}
+            style={styles.iconClose}
+            color={theme.colors.white}
+          />
+          <Text style={styles.title}>Comece a criar agora</Text>
+        </View>
+      </View>
+    </BottomSheet>
+  )
+})
